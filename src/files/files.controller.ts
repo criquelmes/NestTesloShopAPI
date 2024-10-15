43fd39fabdbe305ad2a93886f2add0aa407b38b8
Christopher Riquelme <ccriquelmes@gmail.com>
2024-10-15 16:15:04 -0300

    Sección 12: subir archivos de manera controlada

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,7 +1,10 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
@@ -10,10 +13,21 @@ import { FileInterceptor } from '@nestjs/platform-express';
 
 import { diskStorage } from 'multer';
 import { fileFilter, fileNamer } from './helpers';
+import { Response } from 'express';
 
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this.filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -33,8 +47,7 @@ export class FilesController {
     if (!file) {
       throw new BadRequestException('File is not an image');
     }
-    return {
-      filename: file.originalname,
-    };
+    const secureUrl = `${file.filename}`;
+    return { secureUrl };
   }
 }
