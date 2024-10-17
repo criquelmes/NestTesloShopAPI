fc93c11a4eda147d5cdf8f986cb6d305668c2ad3
Christopher Riquelme <ccriquelmes@gmail.com>
2024-10-17 00:47:15 -0300

    Encriptación de contraseña

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,10 @@
-import {
-  BadRequestException,
-  Injectable,
-  InternalServerErrorException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { PostgresExceptionHandler } from 'src/common/exceptions/postgres-handler.exception';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -19,9 +16,15 @@ export class AuthService {
 
   async create(createUserDto: CreateUserDto) {
     try {
-      const user = this.userRepository.create(createUserDto);
+      const { password, ...rest } = createUserDto;
+      const user = this.userRepository.create({
+        ...rest,
+        password: bcrypt.hashSync(password, 10),
+      });
       await this.userRepository.save(user);
+      delete user.password;
       return user;
+      // TODO: Retornar el JWT de acceso
     } catch (error) {
       this.postgresExceptionHandler.handlerDBExceptions(error);
     }
