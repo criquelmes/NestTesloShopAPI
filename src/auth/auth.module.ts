9fbf1593af3d627e83006b9c16e533adbe4da02e
Christopher Riquelme <ccriquelmes@gmail.com>
2024-10-18 01:21:54 -0300

    Sección 13: Nest Authentication - Passport y Modulos Asíncronos

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,11 +5,34 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { PostgresExceptionHandler } from 'src/common/exceptions/postgres-handler.exception';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService, PostgresExceptionHandler],
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log(configService.get('JWT_SECRET'));
+        // console.log(process.env.JWT_SECRET);
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: { expiresIn: '2h' },
+        };
+      },
+    }),
+    // JwtModule.register({
+    //   secret: process.env.JWT_SECRET,
+    //   signOptions: { expiresIn: '2h' },
+    // }),
+  ],
+
   exports: [TypeOrmModule],
 })
 export class AuthModule {}
