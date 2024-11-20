456dfc15be09adecd13276fc465e79b3567c4c29
Christopher Riquelme <ccriquelmes@gmail.com>
2024-11-20 01:21:21 -0300

    Sección 13: Verificar rol del usuario

--- src/auth/guards/user-role/user-role.guard.ts
+++ src/auth/guards/user-role/user-role.guard.ts
@@ -1,6 +1,13 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { User } from 'src/auth/entities/user.entity';
 
 @Injectable()
 export class UserRoleGuard implements CanActivate {
@@ -9,7 +16,18 @@ export class UserRoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const roles = this.reflector.get<string[]>('roles', context.getHandler());
-    console.log(roles);
-    return true;
+    if (!roles) return true;
+    if (roles.length === 0) return true;
+    const req = context.switchToHttp().getRequest();
+    const user = req.user as User;
+
+    if (!user) throw new BadRequestException('User not found');
+
+    for (const role of user.roles) {
+      if (roles.includes(role)) return true;
+    }
+    throw new ForbiddenException(
+      'You do not have permission to access this route',
+    );
   }
 }
