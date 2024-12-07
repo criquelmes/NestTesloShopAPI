f17b91d4b44d8f7d186193f7172802d15dc3f22e
Christopher Riquelme <ccriquelmes@gmail.com>
2024-12-07 01:12:18 -0300

    Sección 14: Nestjs swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,6 +13,15 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo RESTFul API')
+    .setDescription('Teslo shop endpoint')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('_docs', app, documentFactory);
+
   await app.listen(process.env.PORT);
   logger.log(`Server is running on port ${process.env.PORT}`);
 }
