b54cadbedd599d653d787c77786277c3eb15a256
Christopher Riquelme <ccriquelmes@gmail.com>
2025-01-01 18:37:21 -0300

    Sección 15: Server - Mantener identificados los clientes

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -13,9 +13,11 @@ export class MessagesWsGateway
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+    this.messagesWsService.registerClient(client);
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
   handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+    this.messagesWsService.removeClient(client.id);
+    console.log({ conectados: this.messagesWsService.getConnectedClients() });
   }
 }
